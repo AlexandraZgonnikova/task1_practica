9add383f5a6913373084af3da003b87114f68df3
Sanchosszza <zgonnikova00@gmail.com>
2022-07-13 23:19:02 +0300

    update

--- task1/task1.cpp
+++ task1/task1.cpp
@@ -1,80 +1,40 @@
 ﻿#include <iostream>
+#include <algorithm>
+#include <vector>
 
-int max(int number1, int number2) {
-    if (number1 > number2) {
-        return number1;
+std::vector<int> weights{ 4, 1, 3 };
+std::vector<int> values{ 4000, 2500, 2000 }; 
+
+int countMax(int maxCapacity)
+{
+    std::vector<std::vector<int>> arr(weights.size() + 1, std::vector<int>(maxCapacity + 1));
+    for (int i = 0; i <= weights.size(); i++)
+    {
+        for (int j = 0; j <= maxCapacity; j++)
+        {
+            if (i == 0 || j == 0)
+            {
+                arr[i][j] = 0;
+            }
+            else
+            {
+                if (weights[i - 1] > j)
+                {
+                    arr[i, j] = arr[i - 1, j];
+                }
+                else
+                {
+                    int prev = arr[i - 1][j];
+                    int byFormula = values[i - 1] + arr[i - 1][j - weights[i - 1]];
+                    arr[i][j] = std::max(prev, byFormula);
+                }
+            }
+        }
     }
-    else return number2;
+    return arr[weights.size()][maxCapacity];
 }
+
 int main()
 {
-    int count;
-    int max_weight;
-    std::cout << "Input max weight ";
-    std::cin >> max_weight;
-    std::cout << "Input max count ";
-    std::cin >> count;
-    int** list = new int* [count + 2];
-    int* cost_list = new int[count + 1];
-    int* weight_list = new int[count + 1];
-    for (int i = 0; i < count + 2; i++) list[i] = new int[max_weight + 2];
-    for (int i = 0; i < count + 2; i++)
-    {
-        for (int j = 0; j < max_weight + 2; j++)
-        {
-            list[i][j] = 0;
-        }
-    }
-    for (int i = 2; i < max_weight + 2; i++)
-    {
-        list[0][i] = i - 1;
-    }
-    for (int i = 2; i < count + 2; i++)
-    {
-        int x;
-        std::cout << "Input " << i - 1 << " weight ";
-        std::cin >> x;
-        weight_list[i] = x;
-        list[i][0] = i - 1;
-        std::cout << "Input " << i - 1 << " cost ";
-        std::cin >> x;
-        cost_list[i] = x;
-    }
-    for (int i = 2; i < count + 2; i++)
-    {
-        for (int j = 2; j < max_weight + 2; j++)
-        {
-            if (j - 1 < weight_list[i])
-            {
-                list[i][j] = list[i - 1][j];
-            }
-            else if (j - 1 >= weight_list[i])
-            {
-                list[i][j] = max(list[i - 1][j], list[i - 1][j - weight_list[i]] + cost_list[i]);
-            }
-        }
-
-    }
-    for (int i = 0; i < count + 2; i++)
-    {
-        for (int j = 0; j < max_weight + 2; j++)
-        {
-            std::cout << list[i][j] << '\t';
-        }
-        std::cout << std::endl;
-    }
-    int i{ count + 1 };
-    int j{ max_weight + 1 };
-    std::cout << "Combination: ";
-    while (i - 1 > 0 and j > 0)
-    {
-        if (list[i - 1][j] == list[i][j])
-        {
-            i--;
-            continue;
-        }
-        std::cout << i - 1 << " ";
-        j -= weight_list[i];
-        i--;
-    }
+    std::cout << countMax(4);
 }
